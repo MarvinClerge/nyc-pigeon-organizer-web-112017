fc3c9bb92e26313713b84f60f5512f88dd4adba8
Marvin Clerge <clergemarvin@gmail.com>
2017-11-17 14:57:30 +0000

    Done

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,48 @@
+require 'pry'
+
+# pigeon_data = {
+#   :color => {
+#     :purple => ["Theo", "Peter Jr.", "Lucky"],
+#     :grey => ["Theo", "Peter Jr.", "Ms. K"],
+#     :white => ["Queenie", "Andrew", "Ms. K", "Alex"],
+#     :brown => ["Queenie", "Alex"]
+#   },
+#   :gender => {
+#     :male => ["Alex", "Theo", "Peter Jr.", "Andrew", "Lucky"],
+#     :female => ["Queenie", "Ms. K"]
+#   },
+#   :lives => {
+#     "Subway" => ["Theo", "Queenie"],
+#     "Central Park" => ["Alex", "Ms. K", "Lucky"],
+#     "Library" => ["Peter Jr."],
+#     "City Hall" => ["Andrew"]
+#   }
+# }
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  arr = {}
+
+  #create our nested hashes and array with iteration
+  data.each do |cata, c_hash| #:gender, {:purple=>["Theo", "Peter Jr.", "Lucky"], etc}
+    c_hash.each do |prop, p_array| #:male, ["Theo", etc]
+      p_array.map do |x| #"Theo"
+        arr[x] ||= {}
+        arr[x][cata] ||= []
+      end
+    end
+  end
+
+  #if the catagory of the array is empty create first index, else push into array
+  data.each do |cata, c_hash| #:gender, {:purple=>["Theo", "Peter Jr.", "Lucky"], etc}
+    c_hash.each do |prop, p_array| #:male, ["Theo", etc]
+      p_array.each do |x,i| #"Theo"
+        if arr[x][cata].empty?
+          arr[x][cata] = [prop.to_s]
+        else
+          arr[x][cata] << prop.to_s
+        end
+      end
+    end
+  end
+  arr
 end
